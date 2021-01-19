e3075ffdd3ea17601406fc0364cb15e78a4e481d
jasonwang7517 <jason.wang7517@gmail.com>
2021-01-18 19:56:00 -0500

    1/18 commit

---
+++ luckyNumbers.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def luckyNumbers (self, matrix):
+        ans = []
+        rowMin = {}
+        colMax = {}
+        for i in range(0, len(matrix)):
+            rowMin[i] = 100001
+        for i in range (0, len(matrix[0])):
+            colMax[i] = 0
+        for i in range(0, len(matrix)):
+            for j in range (0, len(matrix[i])):
+                currentElement = matrix[i][j]
+                if currentElement > colMax[j]:
+                    colMax[j] = currentElement
+                if currentElement < rowMin[i]:
+                    rowMin[i] = currentElement
+        for i in range(0, len(matrix)):
+            for j in range (0, len(matrix[i])):
+                if rowMin[i] == colMax[j]:
+                    ans.append(rowMin[i])
+        return ans
