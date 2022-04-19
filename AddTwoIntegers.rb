547810e91d0766ae21ce04340d4874f743da30ae
jasonwang7517 <jason.wang7517@gmail.com>
2022-04-19 00:32:08 -0700

    4/19 commit

---
+++ AddTwoIntegers.rb
@@ -0,0 +1,8 @@
+=begin
+Given two integers num1 and num2, return the sum of the two integers.
+
+=end
+
+def sum(num1, num2)
+    return num1 + num2
+end
---
+++ RootEqualsSumofChildren.rb
@@ -0,0 +1,26 @@
+=begin
+
+You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise.
+
+=end
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Boolean}
+
+def check_tree(root)
+    if root::left.nil? or root::right.nil?
+        return false
+    end
+    return root::val == (root::left::val + root::right::val)
+end
