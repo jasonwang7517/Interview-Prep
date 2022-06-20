39ff79a033fea292f2c617a687139a8eacf1f6be
jasonwang7517 <jason.wang7517@gmail.com>
2022-06-20 01:12:49 -0700

    6/20 commit

---
+++ GreatestEnglishLetterInUpperAndLowerCase.py
@@ -0,0 +1,16 @@
+"""
+Given a string of English letters s, return the greatest English letter which occurs as both a lowercase and uppercase letter in s. The returned letter should be in uppercase. 
+If no such letter exists, return an empty string.
+
+An English letter b is greater than another letter a if b appears after a in the English alphabet.
+"""
+
+class GreatestEnglishLetterInUpperAndLowerCase(object):
+    def greatestLetter(self, s):
+        chars = set()
+        for i in s:
+            chars.add(i)
+        for i in range(91, 64, -1):
+            if chr(i) in chars and chr(i + 32) in chars:
+                return chr(i)
+        return ""
---
+++ SummaryRanges.py
@@ -0,0 +1,41 @@
+"""
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there 
+is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+"""
+
+class SummaryRanges(object):
+    def summaryRanges(self, nums):
+        ans = []
+        if len(nums) == 0:
+            return ans
+        if len(nums) == 1:
+            ans.append(str(nums[0]))
+            return ans
+        left = 0
+        right = 1
+        while right < len(nums):
+            while nums[right] - nums[right - 1] <= 1:
+                right += 1
+                if right >= len(nums):
+                    break
+            st = str(nums[left])
+            if right - left > 1:
+                st = st + "->" + str(nums[right - 1])
+            ans.append(st)
+            left = right
+            right += 1
+        if left < len(nums):
+            if right - left <= 1:
+                ans.append(str(nums[left]))
+            else:
+                st = str(nums[left]) + "->" + str(nums[right - 1])
+        return ans
